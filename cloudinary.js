62a2543d8bd846beb47305a7a31ad646140473b9
Niyifrets <adesokan297@gmail.com>
2026-08-18 01:26:20 +0100

    Delete cloudinary.js

--- cloudinary.js
+++
@@ -1,5 +0,0 @@
-export const CLOUDINARY_CLOUD_NAME = "mv4aaxlt";
-export const CLOUDINARY_UPLOAD_PRESET = "ri-eras-kitchen";
-
-export const CLOUDINARY_UPLOAD_URL =
-    `https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/image/upload`;
